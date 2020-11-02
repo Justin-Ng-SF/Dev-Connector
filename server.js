f857a8e1d4c240b266e0f768279d2f5222b8931e
Justin Ng <justinng123409@yahoo.com>
2020-11-01 23:53:50 -0800

    removing warnings

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const connectDB = require('./config/db');
+const path = require('path');//core nodejs module to manipulate paths
 
 const app = express();
 
@@ -9,7 +10,7 @@ connectDB();
 //init middleware, allows user.js to get data, ie use req.body
 app.use(express.json({ extended: false }));
 
-app.get('/', (req, res) => res.send('API Running'));
+//app.get('/', (req, res) => res.send('API Running'));
 
 //define routes
 app.use('/api/users', require('./routes/api/users'));
@@ -17,7 +18,17 @@ app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/profile', require('./routes/api/profile'));
 app.use('/api/posts', require('./routes/api/posts'));
 
+//serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+    //set static folder
+    app.use(express.static('client/build'));
+    //serve html file
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    })
 
+
+}
 
 
 const PORT = process.env.PORT || 5000;
